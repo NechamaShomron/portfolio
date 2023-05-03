54c6dfd8ad5db48bc3809eb653fea7542ec10ed4
nechama <nechamasho@gmail.com>
2023-05-03 12:45:53 +0300

    header complete

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
-// import styles from './Header.module.css';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styles from './Header.module.css';
 
 export default function Header() {
+  const [showMobile, setShowMobile] = useState(false);
+  const logoImage = '/logo.PNG';
+  function toggleMobileView() {
+    setShowMobile(!showMobile);
+  }
+  function closeMobileView(e) {
+    setShowMobile(false);
+    e.stopPropagation();
+  }
   return (
-    <h1>hi</h1>
+    <>
+      <nav className={styles.navbar}>
+        <Image className={styles.logo_image} role="presentation" loader={() => logoImage} src={logoImage} alt="logo" width={40} height={40} unoptimized priority />
+        <ul className={showMobile ? `${styles.navbar_menu} ${styles.active}` : styles.navbar_menu}>
+          <li className={styles.navbar_item} role="presentation" onClick={toggleMobileView}>
+            <a href="/" className={styles.navbar_link}>Home</a>
+          </li>
+          <li className={styles.navbar_item} role="presentation" onClick={toggleMobileView}>
+            <a href="/about" className={styles.navbar_link}>About</a>
+          </li>
+          <li className={styles.navbar_item} role="presentation" onClick={toggleMobileView}>
+            <a href="/projects" className={styles.navbar_link}>Projects</a>
+          </li>
+          <li className={styles.navbar_item} role="presentation" onClick={toggleMobileView}>
+            <a href="/contact" className={styles.navbar_link}>Contact</a>
+          </li>
+        </ul>
+        <div
+          role="presentation"
+          className={styles.hamburger_container}
+          onClick={toggleMobileView}
+        >
+          <span className={showMobile ? `${styles.hamburger_bar} ${styles.active}` : styles.hamburger_bar} />
+          <span className={showMobile ? `${styles.hamburger_bar} ${styles.active}` : styles.hamburger_bar} />
+          <span className={showMobile ? `${styles.hamburger_bar} ${styles.active}` : styles.hamburger_bar} />
+        </div>
+      </nav>
+      <div className={styles.hero_section_container} role="presentation" onClick={(e) => closeMobileView(e)} />
+    </>
   );
 }
