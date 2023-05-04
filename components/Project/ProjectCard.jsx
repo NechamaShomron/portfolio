b4abcf9f6a935e1584d232bfa068fe4788c052f1
nechama <nechamasho@gmail.com>
2023-05-04 18:21:23 +0300

    projects page

---
+++ components/Project/ProjectCard.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styles from './ProjectCard.module.css';
+
+export default function ProjectCard({ title, description, technologies }) {
+  const technologiesArr = technologies.split(' ');
+
+  return (
+    <div className={styles.project_container}>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.description}>
+        {description}
+      </div>
+      <div className={styles.technologies_container}>
+        {technologiesArr && technologiesArr.map((technology) => (
+          <span className={styles.technology} key={technology}>{technology}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import getLocalData from '../../lib/localdata';
+import ProjectCard from '../../components/Project/ProjectCard';
+import styles from '../styles/Projects.module.css';
 
-export default function Projects() {
+// eslint-disable-next-line react/prop-types
+export default function Projects({ projects }) {
+  const [projectsList, setProjectsList] = useState(projects);
+
+  useEffect(() => {
+    setProjectsList(projects);
+  }, [projects]);
+
   return (
-    <h1>In Project</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>My Projects</h1>
+      <div className={styles.projects_list}>
+        {projectsList && projectsList.map((project) => (
+          <ProjectCard
+            key={project.Title}
+            title={project.Title}
+            description={project.Description}
+            technologies={project.Technologies}
+          />
+        ))}
+      </div>
+    </div>
   );
+}
+export async function getStaticProps() {
+  const projects = await getLocalData('json/projectsData.json');
+
+  return {
+    props: { projects },
+  };
 }
